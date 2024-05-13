2dbf15325bdfb283fdb47e0cea97e2a97a61f04b
Salz <faisal_animator@yahoo.com>
2024-05-13 20:15:18 +0700

    feat: Add Step 5

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,7 +1,7 @@
 /*
 * Create by : Mochammad Faisal
 * Create at : 2024-05-06 20:04:13
-* Update at : 2024-05-06 20:09:45
+* Update at : 2024-05-13 20:12:46
 */
 
 import 'package:flutter/material.dart';
@@ -45,12 +45,15 @@ class MealItem extends StatelessWidget {
         },
         child: Stack(
           children: [
-            FadeInImage(
-              placeholder: MemoryImage(kTransparentImage),
-              image: NetworkImage(meal.imageUrl),
-              fit: BoxFit.cover,
-              height: 200,
-              width: double.infinity,
+            Hero(
+              tag: meal.id,
+              child: FadeInImage(
+                placeholder: MemoryImage(kTransparentImage),
+                image: NetworkImage(meal.imageUrl),
+                fit: BoxFit.cover,
+                height: 200,
+                width: double.infinity,
+              ),
             ),
             Positioned(
               bottom: 0,
